d35bca6404587e5aadb6696d5daf031d180459d5
slimzim <slimzim@gmail.com>
2020-04-21 13:48:09 -0500

    fulfilled minimum requirements of the assignment with full functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,41 +2,37 @@ console.log("Welcome to Psychic Game!")
 
 var wins = 0
 var losses = 0
+var guessesLeft = 9;
+var guessesSoFar = [];
 
-// CAN THIS ALL BE IN A FUNCTION THAT IS CALLED AFTER EACH ROUND? //
+console.log("Wins:", + " " + wins);
+console.log("Losses:", + " " + losses);
+console.log("Guesses Left:" + " " + guessesLeft);
+    
+document.getElementById("guessesSoFar").innerHTML = guessesSoFar;
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-    var guessesLeft = 9;
-    var guessesSoFar = [];
-    console.log("Wins:", + " " + wins);
-    console.log("Losses:", + " " + losses);
-    console.log("Guesses Left:" + " " + guessesLeft);
-    document.getElementById("guessesSoFar").innerHTML = guessesSoFar;
-    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+var computerLetter = computerChoose();
 
-
-
-    var computerLetter = computerChoose();
-
-    function computerChoose(){
-    var computerChoices =
-        ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var computerLetter = 
-        computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log("computerLetter:", computerLetter);
-        return computerLetter;
+function computerChoose(){
+var computerChoices =
+    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("computerLetter:", computerLetter);
+    return computerLetter;
 }
-
 
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
-    console.log(userGuess)
+    console.log(userGuess);
+    console.log("computerLetter:", computerLetter);
 
     if (userGuess === computerLetter) {
         console.log("Matched!");
         wins++;
         document.getElementById("wins").innerHTML = wins;
         alert("You guessed it! " + computerLetter + " was the correct answer.");
-        restartGame()
+        restartGame();
         }
 
     else if (userGuess !== computerLetter && guessesLeft >= 2){
@@ -61,12 +57,13 @@ document.onkeyup = function(event) {
 }
     
 function restartGame(){
-    console.log("Restarting game...")
-    
-// NEEDS CODE THAT WILL RESET VARIABLES AND MAKE A NEW COMPUTER GUESS
-
-    }
-
-
-
-
+    console.log("Restarting game...");
+    guessesLeft = 9;
+    guessesSoFar = [];
+    document.getElementById("guessesSoFar").innerHTML = guessesSoFar;
+    computerLetter = "";
+    console.log("Wins:", + " " + wins);
+    console.log("Losses:", + " " + losses);
+    console.log("Guesses Left:" + " " + guessesLeft);
+    computerChoose();
+}
